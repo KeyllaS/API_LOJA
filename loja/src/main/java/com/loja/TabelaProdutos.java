b7c41c5e23a874d9a5d3b263642b45295ee3b94e
Keylla <keyllasilva@aluno.senai.br>
2023-09-01 09:26:24 -0300

    Enviando arquivo de api loja

--- loja/src/main/java/com/loja/TabelaProdutos.java
+++ loja/src/main/java/com/loja/TabelaProdutos.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TabelaProdutos {
-    List<Produto> produtos = new  ArrayList();
+    List<Produto> produtos = new ArrayList();
 
     public TabelaProdutos() {
-        Produto caneta = new Produto(1, "Caneta BIC" ,
+        Produto caneta = new Produto(1, "Caneta BIC",
                 "Caneta BIC na cor azul", 1.99);
-        Produto borracha = new Produto(2,"Borracha preta" ,
-                "Borracha preta comum" , 3.99);
-        Produto lapis = new Produto(3, "Lápis Faber Castel" ,
-                "Lápis Faber Castel comum" , 2.99);
-        Produto apontador = new Produto(4, "Apontador" ,
-                "Apontador comum com reservatorio" , 7.99);
-        Produto caderno = new Produto(5, "Caderno espiral" ,
-                "Caderno espiral 96 folhas" , 15.99);
+        Produto borracha = new Produto(2, "Borracha preta",
+                "Borracha preta comum", 3.99);
+        Produto lapis = new Produto(3, "Lápis Faber Castel",
+                "Lápis Faber Castel comum", 2.99);
+        Produto apontador = new Produto(4, "Apontador",
+                "Apontador comum com reservatorio", 7.99);
+        Produto caderno = new Produto(5, "Caderno espiral",
+                "Caderno espiral 96 folhas", 15.99);
 
         this.produtos.add(caneta);
         this.produtos.add(borracha);
@@ -29,10 +29,25 @@ public class TabelaProdutos {
     public List<Produto> getProdutos() {
         return produtos;
     }
+
     /**
      * Metódo que retorna todos os produtos da lista
      */
-    public List<Produto> buscarTodosOsProdutos(){
+    public List<Produto> buscarTodosOsProdutos() {
         return this.getProdutos();
     }
+
+    public Produto buscarProdutoPeloId(int produtoId) {
+
+        Produto produtoProcurado = null;
+        for (Produto p : this.produtos) {
+            if (p.getId() == produtoId) {
+                produtoProcurado = p;
+                break;
+            }
+        }
+        return produtoProcurado;
+    }
+
+
 }
